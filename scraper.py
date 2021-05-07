1c8ba736acd70fc490a8b5e3f8abce5ffaab80ae
ThomSullivan <83840319+ThomSullivan@users.noreply.github.com>
2021-05-07 15:46:20 -0500

    split file to seperate the scraper from the bacon sensor

--- scraper.py
+++ scraper.py
@@ -77,67 +77,8 @@ def top_cast(n, movie):
         lst.append(person['name'])
     return lst
 
-#other methods
-def detect_bacon(lst):
-    '''Returns a list of intersections of list with bacon_list'''
-    lst3 = [value for value in lst if value in bacon_list]
-    if len(lst3) > 0:
-        return (True, lst3)
-    else:
-        return False
-
-
-#constant data
-# a list of movies Kevin Bacon is in to start comparisons
-bacon_list = ['Apollo 13', 'Footloose', 'Top Gun']#fill this list!
-
-
+# this part of code is mostly just for output use
 while True:
     person = input("Enter a person's name: ")
     if len(person) == 0:
         break
-    db_name = get_actor_name(person)
-    print(db_name)
-    #first compare top 10 movies to bacon bacon_list
-    degree_zero = top_movies(10, person)
-    print(degree_zero)
-    if detect_bacon(degree_zero) == True:
-        print('Found some bacon!')
-        break
-    #print(check)
-#If nothing found compile list of top ten actors from top ten movies
-    degree_one_actors = []
-    for movie in degree_zero:
-        next_list = top_cast(10, movie)
-        for name in next_list:
-            if name == db_name:
-                continue
-            elif name not in degree_one_actors:
-                degree_one_actors.append(name)
-    #print(degree_one_actors)
-    degree_one_movies = []
-
-    for person in degree_one_actors:
-        try:
-            for movie in top_movies(10, person):
-                if movie in degree_one_movies:
-                    continue
-                elif movie in degree_zero:
-                    continue
-                else:
-                    degree_one_movies.append(movie)
-        except:
-            continue
-
-
-
-
-
-# now detect_bacon on top ten movies of each person on degree_one
-# keep a list, maybe a dictionary would work to store the degree lists
-# {one_movies: [], one_actors:[], two_movies[], two_actors[], ....}
-#    degree_one_actors = []
-#    for actor in degree_one:
-    #    actor = actor.replace(' ', '+')
-
-#        print(top_ten_movies(actor))
