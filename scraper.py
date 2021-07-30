ae03dd6aa2a0bde1b147e79cdccdb3cdeb2d820a
ThomSullivan <83840319+ThomSullivan@users.noreply.github.com>
2021-07-29 21:26:21 -0500

    updates

--- scraper.py
+++ scraper.py
@@ -14,8 +14,7 @@ ctx.verify_mode = ssl.CERT_NONE
 conn = sqlite3.connect('bacon_DB.db')
 cur = conn.cursor()
 count = 0
-id = 1
-api_key = '<put TMdb API key here>'
+api_key = ''#'<put TMdb API key here>'
 
 def json_request(address):
     '''Returns a JSON object from the supplied URL'''
@@ -24,150 +23,43 @@ def json_request(address):
     js = json.loads(data)
     return js
 
-def search_api(type, search):
-    search = urllib.parse.quote_plus(search)
-    address = 'https://api.themoviedb.org/3/search/{}?api_key={}&query={}'.format(type, api_key, search)
-    #print(address)
-    return json_request(address)
-
 def query_get(type, id, get_type):
     address = 'https://api.themoviedb.org/3/{}/{}/{}?api_key={}'.format(type, id, get_type, api_key)
-    #print(address)
+    print(address)
     return json_request(address)
 
-def query_details(type, id):
-    address = 'https://api.themoviedb.org/3/{}/{}?api_key={}'.format(type, id, api_key)
-    #print(address)
-    return json_request(address)
-
-#person methods
-def get_person_id(person):
-    '''Returns the person's ID (passing in a name)'''
-    result = search_api('person', person)
-    person_id = result['results'][0]['id']
-    return person_id
-
-def get_person_name(person):
-    '''Returns a string of the person's name with correct casing'''
-    id = get_person_id(person)
-    result = query_details('person', id)
-    name = result['name']
-    return name
-
-def get_person_rating(person):
-    '''Returns an persons popularity rating(passing in a name) '''
-    id = get_person_id(person)
-    result = query_details('person', id)
-    name = result['popularity']
-    return name
-
-def get_movies(person):
-    '''Returns a list of the movie credits of an person, slice to truncate'''
-    lst = []
-    person_id = get_person_id(person)
-    movies = query_get('person', person_id, 'movie_credits')
-    for i in range(len(movies['cast'])):
-        lst.append(movies['cast'][i]['title'])
-    return lst
-
-#movie methods
-def get_movie_id(movie):
-    '''Returns movie ID'''
-    js = search_api('movie', movie)
-    movie_id = js['results'][0]['id']
-    return movie_id
-
-def get_movie_credits(movie):
-    '''Returns a dictionary of the full credits of a movie requires INT for id'''
-    id = get_movie_id(movie)
-    cast_list = query_get('movie', id, 'credits')
-    return cast_list
-
-def get_cast(movie):
-    '''Returns a list of billed persons, slice to truncate'''
-    lst = []
-    cast = get_movie_credits(movie)
-    for person in cast['cast']:
-        lst.append(person['name'])
-    return lst
-
-req_type = input('What type of request?(person, movie, cast)')
-req_type = req_type.lower()
-
-if req_type == "person":
-    amount = input('person request: Pull how many records?')
-    cur.execute('''SELECT * FROM person ORDER BY id DESC LIMIT 1''')
-    id = cur.fetchone()[0] + 1
-    fail_count = 0
-    while count < int(amount):
-#some ids are invalid
-        try:
-            data = query_details("person", id)
-        except:
-            fail_count = fail_count + 1
-            id = id + 1
-            continue
-
-        conn.commit()
-        count = count + 1
-        print(data['name'],":  ID:", id)
-        cur.execute('''INSERT INTO person (id, name, rating)
-            VALUES ( ?, ?, ? )''', (id, data['name'], data['popularity']))
-        id = id + 1
-    conn.commit()
-    print('Failed requests: ', fail_count)
-
-elif req_type == "movie":
-    amount = input('movie request: Pull how many records?')
-    cur.execute('''SELECT * FROM movie ORDER BY id DESC LIMIT 1''')
-    id = cur.fetchone()[0] + 1
-    fail_count = 0
-    while count < int(amount):
-#some ids are invalid
-        try:
-            data = query_details("movie", id)
-        except:
-            fail_count = fail_count + 1
-            id = id + 1
-            continue
-
-        conn.commit()
-        count = count + 1
-        print(data['title'],":  ID:", id)
-        cur.execute('''INSERT INTO movie (id, title, rating)
-            VALUES ( ?, ?, ? )''', (id, data['title'], data['vote_average']))
-        id = id + 1
-    conn.commit()
-    print('Failed requests: ', fail_count)
-
-elif req_type == 'cast':
-    print('****WARNING each cast request adds many records to local DB*****')
-    amount = input('cast request: Pull how many records?')
-    cur.execute('''SELECT * FROM credits ORDER BY movie_id DESC LIMIT 1''')
+cur.execute('''CREATE TABLE IF NOT EXISTS "credits" (
+	"ID"	INTEGER NOT NULL UNIQUE,
+	"movie_id"	INTEGER NOT NULL,
+	"person_id"	INTEGER NOT NULL,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+    );''')
+amount = input('Pull how many movie credit lists?')
+cur.execute('''SELECT * FROM credits ORDER BY movie_id DESC LIMIT 1''')
+try:
     id = cur.fetchone()[1]
-    print(id)
-    fail_count = 0
+except:
+    id = 0
+id += 1
 
-    while count < int(amount):
+while count < int(amount):
+    try:
+        data = query_get('movie', id, 'credits')
+    except:
+        id += 1
+        continue
+    #some ids are invalid
+    if len(data['cast']) == 0:
         id = id + 1
-#some ids are invalid
-        try:
-            data = query_get('movie', id, 'credits')
-        except:
-            fail_count = fail_count + 1
-            continue
-        count = count + 1
+        continue
+    count = count + 1
+    id = id + 1
 
-        for entry in data['cast']:
-            print('movie ID:', data['id'], ' person name: ', entry['name'], ' Character: ', entry['character'])
-            cur.execute('''INSERT INTO credits (movie_id, person_id, appearing_as)
-                VALUES ( ?, ?, ? )''', (data['id'], entry['id'], entry['character']))
-        for entry in data['crew']:
-            cur.execute('''INSERT INTO credits (movie_id, person_id, appearing_as)
-                VALUES ( ?, ?, ? )''', (data['id'], entry['id'], entry['job']))
-
-        conn.commit()
-    print('Failed requests: ', fail_count)
-
-else:
-    print('error')
+    for entry in data['cast']:
+        print('movie ID:', data['id'], ' person name: ', entry['name'])
+        cur.execute('''INSERT INTO credits (movie_id, person_id)
+            VALUES ( ?, ? )''', (data['id'], entry['id']))
+    for entry in data['crew']:
+        cur.execute('''INSERT INTO credits (movie_id, person_id)
+            VALUES ( ?, ? )''', (data['id'], entry['id']))
+    conn.commit()
--- scraps.py
+++ scraps.py
@@ -1,23 +1,71 @@
-def process_second_degree(person):
-    movies = list_movies(person[0])
-    for movie in movies:
+def query_get(type, id, get_type):
+    address = 'https://api.themoviedb.org/3/{}/{}/{}?api_key={}'.format(type, id, get_type, api_key)
+    #print(address)
+    return json_request(address)
 
-        current_cast = list_cast(movie)
-        current_credits = []
-        for credit in current_cast:
-            tup = (credit[2], person[1], person[0], credit[1])
-            current_credits.append(tup)
-            #print(tup)
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                executor.map(process_cast, current_credits)
-            conn1.commit()
+def search_api(type, search):
+    search = urllib.parse.quote_plus(search)
+    address = 'https://api.themoviedb.org/3/search/{}?api_key={}&query={}'.format(type, api_key, search)
+    #print(address)
+    return json_request(address)
 
-def process_cast(credit):
-    #print(credit)
+def query_get(type, id, get_type):
+    address = 'https://api.themoviedb.org/3/{}/{}/{}?api_key={}'.format(type, id, get_type, api_key)
+    #print(address)
+    return json_request(address)
 
-        #print('Already Exists')
-        return
-    else:
-        #print('made it')
-        print(credit)
-        
+def query_details(type, id):
+    address = 'https://api.themoviedb.org/3/{}/{}?api_key={}'.format(type, id, api_key)
+    #print(address)
+    return json_request(address)
+
+#person methods
+def get_person_id(person):
+    '''Returns the person's ID (passing in a name)'''
+    result = search_api('person', person)
+    person_id = result['results'][0]['id']
+    return person_id
+
+def get_person_name(person):
+    '''Returns a string of the person's name with correct casing'''
+    id = get_person_id(person)
+    result = query_details('person', id)
+    name = result['name']
+    return name
+
+def get_person_rating(person):
+    '''Returns an persons popularity rating(passing in a name) '''
+    id = get_person_id(person)
+    result = query_details('person', id)
+    name = result['popularity']
+    return name
+
+def get_movies(person):
+    '''Returns a list of the movie credits of an person, slice to truncate'''
+    lst = []
+    person_id = get_person_id(person)
+    movies = query_get('person', person_id, 'movie_credits')
+    for i in range(len(movies['cast'])):
+        lst.append(movies['cast'][i]['title'])
+    return lst
+
+#movie methods
+def get_movie_title(movie):
+    '''Returns movie ID'''
+    js = search_api('movie', movie)
+    movie_title = js['results'][0]['title']
+    return movie_id
+
+def get_movie_credits(movie):
+    '''Returns a dictionary of the full credits of a movie requires INT for id'''
+    id = get_movie_id(movie)
+    cast_list = query_get('movie', id, 'credits')
+    return cast_list
+
+def get_cast(movie):
+    '''Returns a list of billed persons, slice to truncate'''
+    lst = []
+    cast = get_movie_credits(movie)
+    for person in cast['cast']:
+        lst.append(person['name'])
+    return lst
